1e9c061cd36155893ef5aabf195019bc5547f420
B4rc1 <0b4rc1@mailbox.org>
2022-10-30 18:59:53 +0100

    fix: dont hardcode nix store path

--- launch_debug.sh
+++ launch_debug.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
-nix-shell --run "VERBOSE=1 ULAUNCHER_WS_API=ws://127.0.0.1:5054/autorandr PYTHONPATH="/nix/store/3asq37jnzdm7cya2q0wz54p6ms9zns5d-ulauncher-5.15.0/lib/python3.9/site-packages:$PYTHONPATH" python3 main.py"
+ULAUNCHER_NIX_STORE_PATH=$(dirname $(dirname "$(readlink $(which ulauncher))"))
+
+nix-shell --run "VERBOSE=1 ULAUNCHER_WS_API=ws://127.0.0.1:5054/autorandr PYTHONPATH="$ULAUNCHER_NIX_STORE_PATH/lib/python3.9/site-packages:$PYTHONPATH" python3 main.py"
