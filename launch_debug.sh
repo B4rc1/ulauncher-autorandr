4b4f7e6d285b8e77d084511effeceba2a8932706
B4rc1 <0b4rc1@mailbox.org>
2022-10-30 19:00:12 +0100

    fix: icons in debug

--- launch_debug.sh
+++ launch_debug.sh
@@ -2,4 +2,4 @@
 
 ULAUNCHER_NIX_STORE_PATH=$(dirname $(dirname "$(readlink $(which ulauncher))"))
 
-nix-shell --run "VERBOSE=1 ULAUNCHER_WS_API=ws://127.0.0.1:5054/autorandr PYTHONPATH="$ULAUNCHER_NIX_STORE_PATH/lib/python3.9/site-packages:$PYTHONPATH" python3 main.py"
+nix-shell --run "VERBOSE=1 ULAUNCHER_WS_API=ws://127.0.0.1:5054/autorandr PYTHONPATH="$ULAUNCHER_NIX_STORE_PATH/lib/python3.9/site-packages:$PYTHONPATH" python3 $(pwd)/main.py"
